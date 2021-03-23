02dcd68155d641cbb1174c044ec7baab077bc2dd
Leandro Jesus Do Nascimento <leandronasx18@gmail.com>
2021-03-23 11:55:47 -0300

    Melhorias media querys, add projetos portfolio

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ const menu = document.getElementById("botao-menu");
 menu.addEventListener("click", function(){
     nav.classList.add("nav-mobile");
 });
+
+window.addEventListener("resize", ()=> {
+    let tamW = window.innerWidth;
+    console.log(tamW);
+    if(tamW > 640){
+        nav.classList.remove("nav-mobile");
+    }
+})
