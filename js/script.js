0d7badd12d368d38f44b362bb33f00be80cbebdd
Leandro Jesus Do Nascimento <leandronasx18@gmail.com>
2022-03-07 23:33:31 -0300

    Colocando menu mobile

--- js/script.js
+++ js/script.js
@@ -1,15 +1,26 @@
-const nav =  document.getElementById("navegacao");
+const cabecalhoMobile =  document.getElementById("cabecalho-menu");
+const navMobile = document.getElementById("cabecalho-navegacao");
+const close = document.getElementById("close-menu");
 const menu = document.getElementById("botao-menu");
 
 
 menu.addEventListener("click", function(){
-    nav.classList.add("nav-mobile");
+    cabecalhoMobile.classList.add("cabecalho-mobile");
+    navMobile.classList.add("nav-mobile");
+    close.classList.remove("oculto");
 });
 
 window.addEventListener("resize", ()=> {
     let tamW = window.innerWidth;
-    console.log(tamW);
-    if(tamW > 640){
-        nav.classList.remove("nav-mobile");
+    if(tamW > 601){
+        cabecalhoMobile.classList.remove("cabecalho-mobile");
+        navMobile.classList.remove("nav-mobile");
+        close.classList.add("oculto");
     }
 })
+
+function fecharMenu(){
+    cabecalhoMobile.classList.remove("cabecalho-mobile");
+    navMobile.classList.remove("nav-mobile");
+    close.classList.add("oculto");
+}
